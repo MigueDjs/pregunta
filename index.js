858c2c3faec164887a56e48acca4708e4c3940f4
Miguel Angel De Jesús Solis <migeholakase@gmail.com>
2024-01-30 00:02:55 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// const yesBtn = document.querySelector('#yesBtn');
+
+// yesBtn.addEventListener('click', function () {
+  //  alert('Sabia que si ibas a querer chikys')
+// })
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*600);
+    const randomY = parseInt(Math.random()*600);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
